0bad7568425c545a0d4e0b9eb64596f72977e1ce
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-05-06 00:13:56 +0530

    Update fetchCategories.js

--- js/fetchCategories.js
+++ js/fetchCategories.js
@@ -1,1 +1,67 @@
 
+const PREVIOUS_POSITION = -1;
+const NEXT_POSITION = 1;
+const SAME_POSITION = 0;
+
+export async function fetchSettingsPageData() {
+  try {
+    const triviaCategories = await fetch(
+      'https://opentdb.com/api_category.php',
+    );
+    const { trivia_categories: categories } = await triviaCategories.json();
+    return {
+      availableCategories: [
+        {
+          id: 'any',
+          name: 'Any',
+        },
+        ...categories.sort((a, b) => {
+          if (a.name < b.name) {
+            return PREVIOUS_POSITION;
+          }
+          if (a.name > b.name) {
+            return NEXT_POSITION;
+          }
+          return SAME_POSITION;
+        }).map((category) => ({
+          id: category.id,
+          name: category.name,
+        })),
+      ],
+      availableDifficulties: [
+        {
+          id: 'any',
+          name: 'Any',
+        },
+        {
+          id: 'easy',
+          name: 'Easy',
+        },
+        {
+          id: 'medium',
+          name: 'Medium',
+        },
+        {
+          id: 'hard',
+          name: 'Hard',
+        },
+      ],
+      availableTypes: [
+        {
+          id: 'any',
+          name: 'Any',
+        },
+        {
+          id: 'multiple',
+          name: 'Multiple Choice',
+        },
+        {
+          id: 'boolean',
+          name: 'True / False',
+        },
+      ],
+    };
+  } catch (error) {
+    return error;
+  }
+}
